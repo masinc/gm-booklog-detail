baccae9e248a5dc67ad0284f42e6b54d71176e6c
masinc <masinc000@gmail.com>
2025-04-13 19:10:48 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,41 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("");
 
-	useEffect(() => {
-		const titleElement = document.querySelector(
-			"div#item-area div.item-info-area > h1",
-		);
-		if (!titleElement) {
-			throw new Error(
-				"div#item-area div.item-info-area > h1 Element is not found",
-			);
-		}
+  useEffect(() => {
+    const titleElement = document.querySelector("div#item-area div.item-info-area > h1");
+    if (!titleElement) {
+      throw new Error("div#item-area div.item-info-area > h1 Element is not found");
+    }
 
-		const title = titleElement.textContent?.replace("[Kindle]", "") || "";
+    const title = titleElement.textContent?.replace("[Kindle]", "") || "";
 
-		setTitle(title);
-	}, []);
+    setTitle(title);
+  }, []);
 
-	const buttons = [
-		{
-			text: "Bookoff (Yahoo)",
-			url: `https://store.shopping.yahoo.co.jp/bookoffonline/search.html?X=4&p=${title}`,
-		},
+  const buttons = [
+    {
+      text: "Bookoff (Yahoo)",
+      url: `https://store.shopping.yahoo.co.jp/bookoffonline/search.html?X=4&p=${title}`,
+    },
+    {
+      text: "bookfan (Yahoo)",
+      url: `https://store.shopping.yahoo.co.jp/bookfan/search.html?X=4&p=${title}`,
+    },
+  ];
 
-		{
-			text: "bookfan (Yahoo)",
-			url: `https://store.shopping.yahoo.co.jp/bookfan/search.html?X=4&p=${title}`,
-		},
-	];
-
-	return (
-		<>
-			{buttons.map((button) => (
-				<a
-					className="btn"
-					href={button.url}
-					target="_blank"
-					rel="noreferrer"
-					key={button.text}
-				>
-					<button type="button" className="btn text-2xl">
-						{button.text}
-					</button>
-				</a>
-			))}
-		</>
-	);
+  return (
+    <>
+      {buttons.map((button) => (
+        <a className="btn" href={button.url} target="_blank" rel="noreferrer" key={button.text}>
+          <button type="button" className="btn text-2xl">
+            {button.text}
+          </button>
+        </a>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const buyButtonParent = document.querySelector(
-	"div#main div.bookstore-btn-area",
-);
+const buyButtonParent = document.querySelector("div#main div.bookstore-btn-area");
 
 if (!buyButtonParent) {
-	throw new Error("div.btn-others Element is not found");
+  throw new Error("div.btn-others Element is not found");
 }
 
 ReactDOM.createRoot(
-	(() => {
-		const app = document.createElement("div");
-		buyButtonParent.appendChild(app);
-		return app;
-	})(),
+  (() => {
+    const app = document.createElement("div");
+    buyButtonParent.appendChild(app);
+    return app;
+  })(),
 ).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
